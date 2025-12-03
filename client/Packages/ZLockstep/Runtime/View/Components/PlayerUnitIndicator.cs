d13b1602a6778d4dd217c587bc4a653d99371402
alterhz <550360139@qq.com>
2025-12-03 21:47:48 +0800

    feat(prefab): 添加Camp对象并调整子对象层级结构
    - 在Factory.prefab中添加了Camp和Top对象及其组件配置
    - 在MainBase.prefab中将Cube和Flag重命名为Camp
    - 在PowerPlant.prefab中添加了Camp对象及两个Top子对象
    - 在Stope.prefab中添加了Camp父对象并重新组织子对象层级
    - 在Tank.prefab中添加了Camp对象并更新材质引用和层级顺序
    - 更新了多个预制体中的材质球引用
    - 调整了部分对象的位置和旋转数值精度

--- client/Packages/ZLockstep/Runtime/View/Components/PlayerUnitIndicator.cs
+++ client/Packages/ZLockstep/Runtime/View/Components/PlayerUnitIndicator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using ZLockstep.View.Systems;
 
+
 namespace ZLockstep.View.Components
 {
     /// <summary>
@@ -14,8 +15,42 @@ namespace ZLockstep.View.Components
 
         private void Awake()
         {
-            // 获取所有渲染器组件
-            _renderers = GetComponentsInChildren<Renderer>(true);
+            // 获取Camp子对象的所有渲染器组件（可能在多层嵌套中）
+            Transform campTransform = FindDeepChild(gameObject.transform, "Camp");
+            if (campTransform != null)
+            {
+                _renderers = campTransform.gameObject.GetComponentsInChildren<Renderer>(true);
+            }
+            else
+            {
+                // 获取所有渲染器组件
+                _renderers = GetComponentsInChildren<Renderer>(true);
+                Debug.LogWarning("未能找到名为'Camp'的子对象", this);
+            }
+        }
+
+        /// <summary>
+        /// 深度查找子对象（递归查找所有层级）
+        /// </summary>
+        /// <param name="parent">父级变换组件</param>
+        /// <param name="name">要查找的对象名称</param>
+        /// <returns>找到的变换组件或null</returns>
+        private Transform FindDeepChild(Transform parent, string name)
+        {
+            // 先在直接子对象中查找
+            Transform result = parent.Find(name);
+            if (result != null)
+                return result;
+
+            // 如果没找到，则递归查找每个子对象的子对象
+            foreach (Transform child in parent)
+            {
+                result = FindDeepChild(child, name);
+                if (result != null)
+                    return result;
+            }
+
+            return null;
         }
 
         /// <summary>
