a89dfa1546887f48b76e51c6113fc47f6b497f03
Ziegler <550360139@qq.com>
2026-01-22 16:53:34 +0800

    2012生成exe

--- config/exportConfig.bat
+++ config/exportConfig.bat
@@ -7,6 +7,7 @@
 @ECHO Converting excel files in folder %EXCEL_FOLDER% ...
 for /f "delims=" %%i in ('dir /b /a-d /s %EXCEL_FOLDER%\*.xlsx') do (
     @echo processing %%~nxi
-	@CALL %EXE% --excel %EXCEL_FOLDER%\%%~nxi --json %JSON_FOLDER%\%%~ni.json -p %C_FILE%\%%~ni.cs --header 3 --encoding utf8-nobom --exclude_prefix #
+	@CALL %EXE% --excel %EXCEL_FOLDER%\%%~nxi --json %JSON_FOLDER%\%%~ni.json -p %C_FILE%\%%~ni.cs --sheet
+	rem @CALL %EXE% --excel %EXCEL_FOLDER%\%%~nxi --json %JSON_FOLDER%\%%~ni.json -p %C_FILE%\%%~ni.cs -s --header 3 --encoding utf8-nobom --exclude_prefix # 
 )
 pause
